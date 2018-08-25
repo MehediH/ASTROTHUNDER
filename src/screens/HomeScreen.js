86df604ad40ada0cdf3026ca99423b4e7eaf9916
Mehedi Hassan <mehedi.dip@outlook.com>
2018-08-25 16:15:20 +0100

    how u feel

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,15 +4,24 @@ import { Link } from 'react-router-dom';
 export default class HomeScreen extends Component {
   render() {
     return (
-        <div className="wrapper">
-            <header>
-                <h1><Link to="/">built by meh</Link></h1>
-                <div>
-                    <span></span><span class="alt"></span>
-                </div>
-            </header>
+        <div className="app">
+            <div className="wrapper">
+                <header>
+                    <h1><Link to="/">built by meh</Link></h1>
+                    <div>
+                        <span></span><span class="alt"></span>
+                    </div>
+                </header>
+                
+                
             
-        
+            </div>
+
+            <div className="background-overlay">
+                {
+                    [...Array(50)].map((e, i) => <span></span>)
+                }
+            </div>
         </div>
     )
   }
