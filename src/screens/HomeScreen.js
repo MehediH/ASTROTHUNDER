ddd740ae97ebe88be15aa741475d16c19d54934e
Mehedi Hassan <mehedi.dip@outlook.com>
2018-08-25 23:18:16 +0100

    3500

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -34,7 +34,6 @@ export default class HomeScreen extends Component {
                     </div>
                 </div>
 
-                <span dangerouslySetInnerHTML={{__html: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1799" height="344"><g><defs><path d="M 0 330 L 223.578 126 L 466.966 330 L 563.19 164 L 638.659 330 L 774.504 0 L 1126 344 L 1293.356 50 L 1533.914 305 L 1717.87 20 L 1799 60" id="a1000z"></path><filter id="a1006z" x="-1.4%" y="-7.1%" width="102.5%" height="112.9%" filterUnits="objectBoundingBox"><feOffset dx="5" dy="5" in="SourceAlpha" result="a1008z"></feOffset><feGaussianBlur stdDeviation="0" in="a1008z" result="a1009z"></feGaussianBlur><feColorMatrix color-interpolation-filters="sRGB" values="0 0 0 0 0.725   0 0 0 0 0.082   0 0 0 0 0.337  0 0 0 1 0" type="matrix" in="a1009z" result="a1010z"></feColorMatrix></filter></defs><g filter="url(#a1006z)"><use stroke-width="9" stroke="black" fill="black" fill-opacity="0" stroke-opacity="1" xlink:href="#a1000z" clip-path="url(#a1001z)"></use></g><use xlink:href="#a1000z" fill="transparent" clip-path="url(#a1001z)" stroke-width="9" stroke="#7755CC"></use></g></svg>'}} />
             </div>
 
             <div className="background-overlay">
