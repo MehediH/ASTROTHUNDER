c41f709fe85af91885f7f24559e8559788f8c631
Mehedi Hassan <mehedi.dip@outlook.com>
2018-08-28 13:58:06 +0100

    purity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
 export default class HomeScreen extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+        gridSizeX: 0,
+        gridSizeY: 0
+    }
+
+  }
+
+  componentWillMount(){
+    this.setState({
+        gridSizeX: Math.round(window.innerWidth / 256),
+        gridSizeY: Math.round(window.innerHeight / 288)
+    })
+    
+    window.addEventListener('resize', () => {        
+        this.setState({
+            gridSizeX: Math.round(window.innerWidth / 256),
+            gridSizeY: Math.round(window.innerHeight / 288)
+        })
+
+    })
+  }
+
   render() {
     return (
         <div className="app">
@@ -36,10 +62,10 @@ export default class HomeScreen extends Component {
 
             </div>
 
-            <div className="background-overlay" style={{gridTemplateColumns: "repeat(" + Math.round(window.innerWidth / 256) + ", 1fr"}}>
+            <div className="background-overlay anim" style={{gridTemplateColumns: "repeat(" + this.state.gridSizeX + ", 1fr"}}>
                 {   
 
-                    [...Array(Math.round(window.innerWidth / 256) * Math.round(window.innerHeight / 288))].map((e, i) => <span key={i}></span>)
+                    [...Array(this.state.gridSizeX * this.state.gridSizeY)].map((e, i) => <span key={i}></span>)
                 }
             </div>
         </div>
