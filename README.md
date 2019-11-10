c95a19b1969c9984a6f695f8df687301e8e9755d
Mehedi Hassan <mehedi.dip@outlook.com>
2019-11-10 23:22:17 +0000

    Revert "hi"
    This reverts commit b874b62f8c4a1751227241cd29ab3988d30fad25.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# coffeebean
+# ASTROTHUNDER
 
 [![screenshot of builtbymeh.com](https://images.ctfassets.net/f1nnr97nijby/5LEmTAK4qAKQKiE6yAiE0K/c48df31bc72fee9ec0037fab67635b5f/astro.jpg)](https://builtbymeh.com)
 
 
-ASTROTUHNDER is the website for my new portfolio. I had a very simple portfolio in the past and didn't feature much of my work or exactly what I do there. coffeebean is a full-blown PWA app based on React and uses Contentful for the content delivery.
+ASTROTUHNDER is the website for my new portfolio. I had a very simple portfolio in the past and didn't feature much of my work or exactly what I do there. ASTROTHUNDER is a full-blown PWA app based on React and uses Contentful for the content delivery.
 
-The design of the app was the main focus. If you are a hip-hop fan like me, you may realise that the project's name is a song from Travis Scott's Astroworld album, and coffeebean happens to be one of my favourite tracks off the tape. The main inspiration for the design came from the "When you hustle, always make it fancy" verse on the STOP TRYING TO BE GOD track, and that's why a lot of the design elements you see on the site are not something you would usually see around the web. I wanted the design of the site to stand out from every other portfolio you see on the internet. This was a tiny side project for me, so I worked on it over the months. And the end product is a design that's equally weird and unique.
+The design of the app was the main focus. If you are a hip-hop fan like me, you may realise that the project's name is a song from Travis Scott's Astroworld album, and ASTROTHUNDER happens to be one of my favourite tracks off the tape. The main inspiration for the design came from the "When you hustle, always make it fancy" verse on the STOP TRYING TO BE GOD track, and that's why a lot of the design elements you see on the site are not something you would usually see around the web. I wanted the design of the site to stand out from every other portfolio you see on the internet. This was a tiny side project for me, so I worked on it over the months. And the end product is a design that's equally weird and unique.
 
 Feel free to fork this project and put your own twist to it. It's an ordinary React app that you can install with `yarn install` so go ahead and play around with things :) 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,109 +3,76 @@ import { Link } from 'react-router-dom';
 import { generateBG } from '../utils/bgAnim.js';
 import {Helmet} from "react-helmet";
 
+export default class HomeScreen extends Component {
+  componentWillMount(){
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var elemProps = generateBG(width, height, false);
 
+    this.setState({
+        ...elemProps
+    })
 
-import * as contentful from 'contentful';
-
-var client = contentful.createClient({
-    space: process.env.REACT_APP_BLOG_SPACE,
-    accessToken: process.env.REACT_APP_BLOG_ACCESS
-})
-
-
-export default class ProjectScreen extends Component {
-    constructor(props){
-        super(props)
-
-        this.state = {
-            projects: [],
-            gridSizeX: 0,
-            gridSizeY: 0
+    window.addEventListener('resize', () => {
+        if(width !== window.innerWidth || height !== window.innerHeight){
+            var elemProps = generateBG(window.innerWidth, window.innerHeight, true);
+    
+            this.setState({
+                ...elemProps
+            })
         }
-    } 
+    })
+  }
 
-    componentWillMount(){
-        var width = window.innerWidth;
-        var height = window.innerHeight;
-        var elemProps = generateBG(width, height, false);
+  render() {
+    return (
+        <React.Fragment>
+            <Helmet>
+                <title>built by meh.</title>
+                <meta name="description" content="A creative designer and developer based in London." />
+            </Helmet>
 
-        this.setState({
-            ...elemProps
-        })
-
-        window.addEventListener('resize', () => {
-            if(width !== window.innerWidth){
-                var elemProps = generateBG(window.innerWidth, window.innerHeight, true);
-        
-                this.setState({
-                    ...elemProps
-                })
-            }
-        })
-
-    }
-
-    componentDidMount(){
-        client.getEntries({
-            content_type: "project"
-
-            
-        }).then(entries => {
-            console.log(entries);
-            this.setState({projects: entries.items});
-        }) 
-    }
-
-
-    render() {
-        const projects = this.state.projects;
-        return (
-            <React.Fragment>
-                <Helmet>
-                    <title>Emily Ko</title>
-                    <meta name="description" content="Photographer based in London." />
-                </Helmet>
+            <div className="wrapper">
+                <header>
+                    <Link to="/"><h1>built by meh.</h1></Link>
+                    <div>
+                        <span></span><span className="alt"></span>
+                    </div>
+                </header>
                 
-                <div className="wrapper">
-                    <header>
-                        <h1><Link to="/">Emily Ko.</Link></h1>
-                        <ul>
-                            <li><a href="https://www.linkedin.com/in/chui-lam-emily-k-b3920b159/">LinkedIn</a></li>
-                            <li><a href="mailto:emily_ko@live.co.uk">Email</a></li>
-                        </ul>
-                    </header>
-                    
-                    <div className="site-cont projects">
-                        {
-                            projects.map((project) =>
-                                project.fields.visibleToPublic ? 
-                                    <article key={project.sys.id}>
-                                            <Link to={"./projects/" + project.fields.slug} className="project">
-                                                <div className="container">
-                                                    <img src={project.fields.coverImage.fields.file.url}/>
-                                                    <div className="overlay"><h1>{project.fields.title}</h1></div>
-                                                    
-                                                </div>
-
-                                            </Link>
-                
-                                            
-                                    </article>  
-                                : <div> </div>
-                                
-                            )
-                        }
+                <div className="main site-cont">
+                    <div className="hero">
+                        <h1 className="hi">hi! i'm mehedi.</h1>
+                        <h1>a creative developer, designer based in london.</h1>
+                        <h2>i build beautiful, robust, and effortless websites, apps, and other web-based experiences.</h2>
+                        <Link to="/services" className="services card">see what i do <span className="arrow">-></span></Link>
                     </div>
 
-                </div>
-                
-                <div className={"background-overlay anim " + this.state.resize } style={{gridTemplateColumns: "repeat(" + this.state.gridSizeX + ", 1fr"}}>
-                    {   
+                    <div className="children">
+                        <Link to="./projects">
+                            <div className="projects card">
+                                <h2>view my projects</h2>
+                            </div>
+                        </Link>
 
-                        [...Array(this.state.gridSizeX * this.state.gridSizeY)].map((e, i) => <span key={i}></span>)
-                    }
+                        <Link to="./contact">
+                            <div className="contact card">
+                                <h2>let's work together</h2>
+                            </div>
+                        </Link>
+                    </div>
                 </div>
-            </React.Fragment>   
-        )
-    }
+
+            </div>
+
+            <div className={"background-overlay anim " + this.state.resize } style={{gridTemplateColumns: "repeat(" + this.state.gridSizeX + ", 1fr"}}>
+                {   
+
+                    [...Array(this.state.gridSizeX * this.state.gridSizeY)].map((e, i) => <span key={i}></span>)
+                }
+            </div>
+            
+           </React.Fragment>
+    )
+  }
 }
