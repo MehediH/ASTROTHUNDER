546a954032776ac6a5b4b4e1b2534c974b6b5c5f
Mehedi Hassan <mehedi.dip@outlook.com>
2020-05-07 03:28:28 +0100

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -50,7 +50,7 @@ export default class HomeScreen extends Component {
                     <div className="hero">
                         <h1 className="hi">hi! i'm mehedi.</h1>
                         <h1>a software engineer, compsci undergrad, and designer based in london.</h1>
-                        <h2>i love building beautiful, functional software that solve problems, and designing intuitive experiences.</h2>
+                        <h2>i like making beautiful, functional products designed to solve problems.</h2>
                         <Link to={{pathname: "/contact", state: { ...this.props.location.state, comeThru: true}}} className="services card">more about me <span className="arrow">-></span></Link>
                     </div>
 
